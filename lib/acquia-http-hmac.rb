a4780b785f5909deef106ace4279fa368bb3ede5
peter.wolanin <peter.wolanin@acquia.com>
2015-05-21 12:34:42 -0400

    add method & fix ws

--- lib/acquia-http-hmac.rb
+++ lib/acquia-http-hmac.rb
@@ -4,54 +4,63 @@ require 'securerandom'
 require 'uri'
 
 module Acquia
-	class HTTPHmac
-	  VERSION = '2.0'
+  class HTTPHmac
+    VERSION = '2.0'
 
-	  # These accessors are normally just for unit testing
-	  attr_accessor :nonce, :timestamp
+    # These accessors are normally just for unit testing
+    attr_accessor :nonce, :timestamp
 
-		def initialize(realm, secret)
-		  @realm = realm
-		  @secret = secret
-		end
+    def initialize(realm, secret)
+      @realm = realm
+      @secret = secret
+    end
 
-		def prepare_request(http_method, host, id, path_info= '/', query_string = '', body = '', content_type = '')
-  		http_method = http_method.upcase
-		  base_string_parts = [http_method, host, path_info]
-		  @timestamp ||= "%0.6f" % Time.now.to_f
-		  @nonce ||= SecureRandom.uuid
-		  base_string_parts << "id=#{URI.escape(id)}&nonce=#{nonce}&realm=#{URI.escape(@realm)}&timestamp=#{timestamp}&version=#{VERSION}"
+    def prepare_request(http_method, host, id, path_info= '/', query_string = '', body = '', content_type = '')
+      http_method = http_method.upcase
+      base_string_parts = [http_method, host, path_info]
+      @timestamp ||= "%0.6f" % Time.now.to_f
+      @nonce ||= SecureRandom.uuid
+      base_string_parts << "id=#{URI.escape(id)}&nonce=#{nonce}&realm=#{URI.escape(@realm)}&timestamp=#{timestamp}&version=#{VERSION}"
       if ['GET', 'HEAD'].include?(http_method)
         unless query_string.empty?
           base_string_parts << normalize_query(query_string)
         end
-		  elsif !body.empty?
-		    # Body hash
-  		  base_string_parts << Digest::SHA256.base64digest(body)
-  		  base_string_parts << content_type.downcase
-  		end
-  		base_string = base_string_parts.join("\n")
+      elsif !body.empty?
+        # Body hash
+        base_string_parts << Digest::SHA256.base64digest(body)
+        base_string_parts << content_type.downcase
+      end
+      base_string = base_string_parts.join("\n")
 
-		  authorization = []
-		  authorization << "acquia-http-hmac realm=\"#{@realm}\""
-		  authorization << "id=\"#{id}\""
-		  authorization << "timestamp=\"#{@timestamp}\""
-		  authorization << "nonce=\"#{@nonce}\""
-		  authorization << "version=#{VERSION}"
-		  authorization << "signature=\"#{signature(base_string)}\""
+      authorization = []
+      authorization << "acquia-http-hmac realm=\"#{@realm}\""
+      authorization << "id=\"#{id}\""
+      authorization << "timestamp=\"#{@timestamp}\""
+      authorization << "nonce=\"#{@nonce}\""
+      authorization << "version=#{VERSION}"
+      authorization << "signature=\"#{signature(base_string)}\""
       authorization.join(',')
-		end
+    end
 
-		def valid_request?(http_method, host, path_info, authorization_header, query_string = '', body = '', content_type = '')
-		  false
-		end
+    def valid_request?(http_method, host, path_info, authorization_header, query_string = '', body = '', content_type = '')
+      false
+    end
 
-		def normalize_query(query_string)
-		  query_string
-		end
+    def parse_header(header)
+      attributes = {}
+      header.to_s.sub(/^acquia-http-hmac\s+/, '').split(/,\s*/).each do |value|
+        m = value.match(/^(\w+)\=\"([^\"]*)\"$/)
+        attributes[m[1].to_sym] = URI.decode(m[2]))
+      end
+      attributes
+    end
 
-		def signature(base_string)
-		  Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, @secret, base_string)).strip
-		end
-	end
+    def normalize_query(query_string)
+      query_string
+    end
+
+    def signature(base_string)
+      Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA256.new, @secret, base_string)).strip
+    end
+  end
 end
